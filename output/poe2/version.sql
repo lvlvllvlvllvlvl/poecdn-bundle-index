16a92d8698d5dbe266f0a8e5f6ae752dc35e7d5c
lvlvllvlvllvlvl <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-06-11 12:20:27 +0000

    Apply automatic changes

--- output/poe2/version.sql
+++ output/poe2/version.sql
@@ -1,1 +1,1 @@
-INSERT INTO "version" ("id", "url") VALUES (0, 'https://patch-poe2.poecdn.com/4.2.1.1.3/');
+INSERT INTO "version" ("id", "url") VALUES (0, 'https://patch-poe2.poecdn.com/4.2.1.1.4/');
