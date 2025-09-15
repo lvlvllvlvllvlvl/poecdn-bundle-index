40ccb2f4baea9fd4177277a6a615614fd88b017f
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-09-15 08:30:35 +0700

    remove redundant index, recreate db

--- sql/create_indexes.sql
+++ sql/create_indexes.sql
@@ -1,5 +1,4 @@
 CREATE UNIQUE INDEX bundle_name ON bundles (name);
 CREATE UNIQUE INDEX dir_name ON dirs (name);
-CREATE UNIQUE INDEX file_hash ON files (hash);
 CREATE INDEX dir_parent ON dirs (parent);
 CREATE INDEX file_path ON files (dir, name);
