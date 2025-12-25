d32ef446fee550fc2f680da86e05ea18726a6d0b
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-12-25 12:54:40 +0700

    skip better

--- scripts/generate_sql.sh
+++ scripts/generate_sql.sh
@@ -14,21 +14,28 @@ OUTFILE="${GAME}.sql"
 GAME_DIR="output/${GAME}"
 UPDATE_FILE=("${GAME_DIR}"/update-*.sql)
 
+ls -lah "$GAME_DIR"
+
 rebuild_from_scratch() {
-  ls "$GAME_DIR"
-  echo "PRAGMA defer_foreign_keys = on;" > "${OUTFILE}"
-  cat sql/{drop,create}_tables.sql >> "${OUTFILE}"
-  cat "${GAME_DIR}"/{bundles,dirs,files,version}.sql >> "${OUTFILE}"
-  cat sql/create_indexes.sql >> "${OUTFILE}"
+  {
+    echo "PRAGMA defer_foreign_keys = on;"
+    cat sql/{drop,create}_tables.sql \
+      "${GAME_DIR}"/{bundles,dirs,files,version}.sql \
+      sql/create_indexes.sql
+  } > "${OUTFILE}"
+  ls -lah "${OUTFILE}"
 }
 
 if [[ "${FORCE_REBUILD}" == "true" ]]; then
   echo "Forced rebuild for ${GAME} via workflow_dispatch input"
   rebuild_from_scratch
-elif [[ -f "$UPDATE_FILE" ]]; then
-  echo "Using update script for ${GAME}: ${UPDATE_FILE[*]}"
+elif [[ -f "${GAME_DIR}"/update-not-required.sql ]]; then
+  echo update not required
+elif [[ -f "${UPDATE_FILE[0]}" ]]; then
+  echo "Using update script for ${GAME}"
   cat "${UPDATE_FILE[@]}" > "${OUTFILE}"
+  ls -lah "${OUTFILE}"
 else
-  echo "Update file $UPDATE_FILE not found. Rebuilding ${GAME} database from scratch"
+  echo "Update file ${UPDATE_FILE[*]} not found. Rebuilding ${GAME} database from scratch"
   rebuild_from_scratch
 fi
